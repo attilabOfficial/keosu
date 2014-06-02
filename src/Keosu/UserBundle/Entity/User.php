0364eac0313422649fad4ce2b4980dbea98a0f86
florent <florent.developpeur.web@gmail.com>
2014-06-02 10:57:55 +0200

    update fos user extends for user.php

--- src/Keosu/UserBundle/Entity/User.php
+++ src/Keosu/UserBundle/Entity/User.php
@@ -2,7 +2,7 @@
 // src/Keosu/UserBundle/Entity/User.php
 
 namespace Keosu\UserBundle\Entity;
-use FOS\UserBundle\Entity\User as BaseUser;
+use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
 /**

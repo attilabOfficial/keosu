04c0fe899b12f4d69b58b174a4995bce0d390b55
florent <florent.developpeur.web@gmail.com>
2014-06-12 18:28:59 +0200

    add reset password to keosu website

---
+++ src/Keosu/Gadget/AuthentificationGadgetBundle/Controller/ResetController.php
@@ -0,0 +1,35 @@
+<?php
+/************************************************************************
+ Keosu is an open source CMS for mobile app
+Copyright (C) 2014  Vincent Le Borgne, Pockeit
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+************************************************************************/
+namespace Keosu\Gadget\AuthentificationGadgetBundle\Controller;
+
+use Keosu\CoreBundle\Util\TemplateUtil;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+use Symfony\Component\HttpFoundation\Request;
+
+
+class ResetController extends Controller {
+
+	public function successAction() {
+		return $this->container->get('templating')->renderResponse('KeosuGadgetAuthentificationGadgetBundle:Resetting:success.html.twig');
+	}
+
+}

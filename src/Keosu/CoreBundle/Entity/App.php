34256506843d9115c6a91df8263cec40cc873715
Badboum <alexandre-marion@hotmail.fr>
2014-07-09 16:56:10 +0200

    Refactoring of config.xml form.
    Creation of a new Entity, ConfigParameters contaning all config.xml's
    parameters.
    Creation of a new form builder for this class, ConfigParametersTypes.
    Creation of a part of the form view for these parameters,
    editConfigParameters.html.twig .

--- src/Keosu/CoreBundle/Entity/App.php
+++ src/Keosu/CoreBundle/Entity/App.php
@@ -20,6 +20,9 @@ namespace Keosu\CoreBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Keosu\CoreBundle\Util\ThemeUtil;
+use Keosu\CoreBundle\Entity\Model\MediaDataModel;
+use Keosu\CoreBundle\Entity\ConfigParameters;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * App
@@ -29,6 +32,21 @@ use Keosu\CoreBundle\Util\ThemeUtil;
  */
 class App
 {
+	
+	/**
+	 * Set default data for the form.
+	 */
+	public function __construct(){
+		$this->packageName = 'com.keosu.demo';
+		$this->description = 'Keosu demo';
+		$this->author = 'keosu team';
+		$this->website = 'http://keosu.com';
+		$this->email = 'vleborgne@keosu.com';
+		$this->theme = array_keys(ThemeUtil::getThemeList())[0];
+		$this->configParam = new ConfigParameters();
+	}
+	
+	
     /**
      * @var integer
      *
@@ -120,170 +138,16 @@ class App
      *
      * @ORM\Column(name="debugMode", type="boolean")
      */
-    private $debugMode;
-    
-    //////////////////////////////////
-    // Preferences for config.xml
-    //////////////////////////////////
-    
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="permissions", type="string", length=1024)
-     */
-    private $permissions;
-    
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="phonegap_version", type="string", length=1024)
-     */
-    private $phonegap_version;
-    
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="orientation", type="string", length=1024)
-     */
-    private $orientation;
-    
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="target_device", type="string", length=1024)
-     */
-    private $target_device;
-    
-    /**
-     * @var boolean
-     *
-     * @ORM\Column(name="fullscreen", type="boolean")
-     */
-    private $fullscreen;
-    
-    /**
-     * @var boolean
-     *
-     * @ORM\Column(name="webviewbounce", type="boolean")
-     */
-    private $webviewbounce;
-    
-    /**
-     * @var boolean
-     *
-     * @ORM\Column(name="prerendered_icon", type="boolean")
-     */
-    private $prerendered_icon;
-    
-    /**
-     * @var boolean
-     *
-     * @ORM\Column(name="stay_in_webview", type="boolean")
-     */
-    private $stay_in_webview;
-    
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="ios_statusbarstyle", type="string", length=1024)
-     */
-    private $ios_statusbarstyle;
-    
-    /**
-     * @var boolean
-     *
-     * @ORM\Column(name="detect_data_types", type="boolean")
-     */
-    private $detect_data_types;
-    
-    /**
-     * @var boolean
-     *
-     * @ORM\Column(name="exit_on_suspend", type="boolean")
-     */
-    private $exit_on_suspend;
-    
-    /**
-     * @var boolean
-     *
-     * @ORM\Column(name="show_splash_screen_spinner", type="boolean")
-     */
-    private $show_splash_screen_spinner;
-    
-    /**
-     * @var boolean
-     *
-     * @ORM\Column(name="auto_hide_splash_screen", type="boolean")
-     */
-    private $auto_hide_splash_screen;
-    
-    /**
-     * @var boolean
-     *
-     * @ORM\Column(name="disable_cursor", type="boolean")
-     */
-    private $disable_cursor;
-    
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="android_minSdkVersion", type="string", length=1024)
-     */
-    private $android_minSdkVersion;
-    
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="android_installLocation", type="string", length=1024)
-     */
-    private $android_installLocation;
-    
-    /**
-     * @var boolean
-     *
-     * @ORM\Column(name="DisallowOverscroll", type="boolean")
-     */
-    private $disallowOverscroll;
-    
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="splash_screen_duration", type="string", length=1024)
-     */
-    private $splash_screen_duration;
-    
-    
-    /**
-     * Set default data for the form.
-     */
-    public function __construct(){
-    	$this->packageName = 'com.keosu.demo';
-    	$this->description = 'Keosu demo';
-    	$this->author = 'keosu team';
-    	$this->website = 'http://keosu.com';
-    	$this->email = 'vleborgne@keosu.com';
-    	$this->theme = array_keys(ThemeUtil::getThemeList())[0];
-    	$this->permissions = 'none';
-    	$this->phonegap_version = '3.1.0';
-    	$this->orientation = "default";
-    	$this->target_device = "universal";
-    	$this->fullscreen = true ;
-    	$this->webviewbounce = true ;
-    	$this->prerendered_icon = true ;
-    	$this->stay_in_webview = false ;
-    	$this->ios_statusbarstyle = 'black-opaque' ;
-    	$this->detect_data_types = true ;
-    	$this->exit_on_suspend = true ;
-    	$this->Show_splash_screen_spinner = true ;
-    	$this->auto_hide_splash_screen = false ;
-    	$this->disable_cursor = false ;
-    	$this->android_minSdkVersion = '7' ;
-    	$this->android_installLocation = 'auto' ;
-    	$this->disallowOverscroll = true ;
-    	$this->splash_screen_duration ='10000' ;
+    private $debugMode;   
 
-    	
-    }
+    
+    /**
+     * @var ConfigParameters
+     * 
+     * @ORM\OneToOne(targetEntity="Keosu\CoreBundle\Entity\ConfigParameters", cascade={"persist"})
+     */
+    private $configParam;
+    
     
     /**
      * Get id
@@ -574,387 +438,73 @@ class App
     {
         return $this->debugMode;
     }
-    
-    ////////////////////////////////////////////////////
-    // Preferences getters / setters
-    ////////////////////////////////////////////////////
-    
+	
     /**
-     * Set permissions
+     * Get configParam
      *
-     * @param string $permissions
-     * @return App
+     * @return ConfigParam
      */
-    public function setPermissions($permissions){
-    	$this->permissions = $permissions;
-    
-    	return $this;
+    public function getConfigParam(){
+    	return $this->configParam;
     }
     
+    
     /**
-     * Get permissions
+     * Set configParam
      *
-     * @return string
+     * @param ConfigParam $configParam
+     * @return App
      */
-    public function getPermissions(){
-    	return $this->permissions;
+    public function setConfigParam(ConfigParameters $configParam){
+    	$this->configParam = $configParam;
+    	return this;
     }
     
-    /**
-     * Set phonegap_version
-     *
-     * @param string $phonegap_version
-     * @return App
-     */   
-	public function setPhonegapVersion($phonegap_version){
-		 $this->phonegap_version = $phonegap_version;
-    
-        return $this;
+	
+	/**
+	 * Set path
+	 *
+	 * @param string $path
+	 * @return ArticleAttachment
+	 */
+	public function setPath($path) {
+		$this->path = $path;
+	
+		return $this;
 	}
 	
 	/**
-	 * Get phonegap_version
+	 * Get path
 	 *
 	 * @return string
 	 */
-	public function getPhonegapVersion(){
-		return $this->phonegap_version;
-	}	
-	
-	/**
-	 * Set orientation
-	 *
-	 * @param string $orientation
-	 * @return App
-	 */
-	public function setOrientation($orientation){
-		$this->orientation = $orientation;
-	
-		return $this;
+	public function getPath() {
+		return $this->path;
 	}
 	
-	/**
-	 * Get orientation
-	 *
-	 * @return string
-	 */
-	public function getOrientation(){
-		return $this->orientation;
+	public function getAbsolutePath() {
+		return null === $this->path ? null
+		: $this->getUploadRootDir() . '/' . $this->path;
 	}
 	
-	/**
-	 * Set fullscreen
-	 *
-	 * @param strin $fullscreen
-	 * @return App
-	 */
-	public function setFullscreen($fullscreen){
-		$this->fullscreen = $fullscreen;
-	
-		return $this;
+	public function getWebPath() {
+		return null === $this->path ? null
+		: '/' . $this->getUploadDir() . '/' . $this->path;
 	}
 	
-	/**
-	 * Get fullscreen
-	 *
-	 * @return string
-	 */
-	public function getFullscreen(){
-		return $this->fullscreen;
+	public function getUploadRootDir() {
+		// the absolute directory path where uploaded
+		// documents should be saved
+		return __DIR__ .'/../../../../web/' . $this->getUploadDir();
 	}
 	
-	/**
-	 * Set target_device
-	 *
-	 * @param string $target_device
-	 * @return App
-	 */
-	public function setTargetDevice($target_device){
-		$this->fullscreen = $target_device;
-	
-		return $this;
+	protected function getUploadDir() {
+		// get rid of the __DIR__ so it doesn't screw up
+		// when displaying uploaded doc/image in the view.
+		return 'uploads/splashscreen';
 	}
 	
-	/**
-	 * Get target_device
-	 *
-	 * @return string
-	 */
-	public function getTargetDevice(){
-		return $this->target_device;
-	}
 	
-	/**
-	 * Set webviewbounce
-	 *
-	 * @param boolean $webviewbounce
-	 * @return App
-	 */
-	public function setWebviewbounce($webviewbounce){
-		$this->webviewbounce = $webviewbounce;
 	
-		return $this;
-	}
-	
-	/**
-	 * Get webviewbounce
-	 *
-	 * @return boolean
-	 */
-	public function getWebviewbounce(){
-		return $this->webviewbounce;
-	}
-	
-	/**
-	 * Set prerendered_icon
-	 *
-	 * @param boolean $prerendered_icon
-	 * @return App
-	 */
-	public function setPrerenderedIcon($prerendered_icon){
-		$this->prerendered_icon = $prerendered_icon;
-	
-		return $this;
-	}
-	
-	/**
-	 * Get prerendered_icon
-	 *
-	 * @return boolean
-	 */
-	public function getPrerenderedIcon(){
-		return $this->prerendered_icon;
-	}
-	
-	/**
-	 * Set stay_in_webview
-	 *
-	 * @param boolean $stay_in_webview
-	 * @return App
-	 */
-	public function setStayInWebview($stay_in_webview){
-		$this->stay_in_webview = $stay_in_webview;
-	
-		return $this;
-	}
-	
-	/**
-	 * Get stay_in_webview
-	 *
-	 * @return boolean
-	 */
-	public function getStayInWebview(){
-		return $this->stay_in_webview;
-	}
-    
-	/**
-	 * Set ios_statusbarstyle
-	 *
-	 * @param string $ios_statusbarstyle
-	 * @return App
-	 */
-	public function setIosStatusbarstyle($ios_statusbarstyle){
-		$this->ios_statusbarstyle = $ios_statusbarstyle;
-	
-		return $this;
-	}
-	
-	/**
-	 * Get ios_statusbarstyle
-	 *
-	 * @return string
-	 */
-	public function getIosStatusbarstyle(){
-		return $this->ios_statusbarstyle;
-	}
-	
-	/**
-	 * Set detect_data_types
-	 *
-	 * @param string $detect_data_types
-	 * @return App
-	 */
-	public function setDetectDataTypes($detect_data_types){
-		$this->detect_data_types = $detect_data_types;
-	
-		return $this;
-	}
-	
-	/**
-	 * Get detect_data_types
-	 *
-	 * @return boolean
-	 */
-	public function getDetectDataTypes(){
-		return $this->detect_data_types;
-	}
-	
-	/**
-	 * Set exit_on_suspend
-	 *
-	 * @param boolean $exit_on_suspend
-	 * @return App
-	 */
-	public function setExitOnSuspend($exit_on_suspend){
-		$this->exit_on_suspend = $exit_on_suspend;
-	
-		return $this;
-	}
-	
-	/**
-	 * Get exit_on_suspend
-	 *
-	 * @return boolean
-	 */
-	public function getExitOnSuspend(){
-		return $this->exit_on_suspend;
-	}
-	
-	/**
-	 * Set show_splash_screen_spinner
-	 *
-	 * @param boolean $show_splash_screen_spinner
-	 * @return App
-	 */
-	public function setShowSplashScreenSpinner($show_splash_screen_spinner){
-		$this->show_splash_screen_spinner = $show_splash_screen_spinner;
-	
-		return $this;
-	}
-	
-	/**
-	 * Get show_splash_screen_spinner
-	 *
-	 * @return boolean
-	 */
-	public function getShowSplashScreenSpinner(){
-		return $this->show_splash_screen_spinner;
-	}
-	
-	/**
-	 * Set auto_hide_splash_screen
-	 *
-	 * @param boolean $auto_hide_splash_screen
-	 * @return App
-	 */
-	public function setAutoHideSplashScreen($auto_hide_splash_screen){
-		$this->auto_hide_splash_screen = $auto_hide_splash_screen;
-	
-		return $this;
-	}
-	
-	/**
-	 * Get auto_hide_splash_screen
-	 *
-	 * @return boolean
-	 */
-	public function getAutoHideSplashScreen(){
-		return $this->auto_hide_splash_screen;
-	}
-	
-	/**
-	 * Set disable_cursor
-	 *
-	 * @param boolean $disable_cursor
-	 * @return App
-	 */
-	public function setDisableCursor($disable_cursor){
-		$this->disable_cursor = $autdisable_cursor;
-	
-		return $this;
-	}
-	
-	/**
-	 * Get disable_cursor
-	 *
-	 * @return boolean
-	 */
-	public function getDisableCursor(){
-		return $this->disable_cursor;
-	}
-	
-	/**
-	 * Set android_minSdkVersion
-	 *
-	 * @param string $android_minSdkVersion
-	 * @return App
-	 */
-	public function setAndroidMinSdkVersion($android_minSdkVersion){
-		$this->android_minSdkVersion = $android_minSdkVersion;
-	
-		return $this;
-	}
-	
-	/**
-	 * Get android_minSdkVersion
-	 *
-	 * @return string
-	 */
-	public function getAndroidMinSdkVersion(){
-		return $this->android_minSdkVersion;
-	}
-	
-	/**
-	 * Set android_installLocation
-	 *
-	 * @param string $android_installLocation
-	 * @return App
-	 */
-	public function setAndroidInstallLocation($android_installLocation){
-		$this->android_installLocation = $android_installLocation;
-	
-		return $this;
-	}
-	
-	/**
-	 * Get android_installLocation
-	 *
-	 * @return string
-	 */
-	public function getAndroidInstallLocation(){
-		return $this->android_installLocation;
-	}
-	
-	/**
-	 * Set disallowOverscroll
-	 *
-	 * @param boolean $DisallowOverscroll
-	 * @return App
-	 */
-	public function setDisallowOverscroll($disallowOverscroll){
-		$this->disallowOverscroll = $disallowOverscroll;
-	
-		return $this;
-	}
-	
-	/**
-	 * Get disallowOverscroll
-	 *
-	 * @return boolean
-	 */
-	public function getDisallowOverscroll(){
-		return $this->disallowOverscroll;
-	}
-	
-	/**
-	 * Set splash_screen_duration
-	 *
-	 * @param string $splash_screen_duration
-	 * @return App
-	 */
-	public function setSplashScreenDuration($splash_screen_duration){
-		$this->splash_screen_duration = $splash_screen_duration;
-	
-		return $this;
-	}
-	
-	/**
-	 * Get splash_screen_duration
-	 *
-	 * @return string
-	 */
-	public function getSplashScreenDuration(){
-		return $this->splash_screen_duration;
-	}
 	
 }

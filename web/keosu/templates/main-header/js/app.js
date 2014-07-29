cb3bc358e2931d7a6af7e3c4757b18277be90e99
florent <florent.developpeur.web@gmail.com>
2014-07-29 11:17:12 +0200

    Remove dead code

--- web/keosu/templates/main-header/js/app.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module('keosuApp', ['angular-chrono', 'angularSpinner', 'ngSanitize', 'ngTouch', 'ngRoute','angular-inview']);
-
-app.controller('main_Controller', function($http, $scope) {
-		$http.get('data/appName.json').success( function (data) {
-			$scope.appName = data.name;
-		}); 
-});

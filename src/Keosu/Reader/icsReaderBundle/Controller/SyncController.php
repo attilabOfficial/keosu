6b5f612671389a3d45014671a0606685dc52de3b
florent <florent.developpeur.web@gmail.com>
2014-08-01 11:36:03 +0200

    replay @f4da47c

--- src/Keosu/Reader/icsReaderBundle/Controller/SyncController.php
+++ src/Keosu/Reader/icsReaderBundle/Controller/SyncController.php
@@ -34,14 +34,13 @@ class SyncController extends Controller {
 	 * Synchronise ics remote contents with articles
 	 * @param $id of the curent reader
 	 */
-	
-	function dist($lat,$lng){
+	private function dist($lat,$lng){
 		$current_lat = 48.117266;
 		$current_lng = -1.6777926;
 		return ($lat-$current_lat)*($lat-$current_lat)+($lng-$current_lng)*($lng-$current_lng);
 	}
 	
-	function cmpByDist($a,$b){
+	private function cmpByDist($a,$b){
 		if ( $this->dist($a['geometry']['location']['lat'],$a['geometry']['location']['lng'])
 		== $this->dist($b['geometry']['location']['lat'],$b['geometry']['location']['lng']) ) {
 			return 0;

14f28ed6a5ac5f9c0d407c0c687f81ed24715377
Badboum <alexandre-marion@hotmail.fr>
2014-08-04 14:39:37 +0200

    Add alert online/offline

--- app/Resources/packages/angularJs/init.js
+++ app/Resources/packages/angularJs/init.js
@@ -4,4 +4,16 @@ app.controller('main_Controller', function($http, $scope) {
 		$http.get('data/appName.json').success( function (data) {
 			$scope.appName = data.name;
 		}); 
+
+		document.addEventListener("offline",
+		        function(){
+					$scope.offline=true;
+		            alert("Perte du r&eacute;seau. La page sera recharg&eacute;e; d&eacute;s que possible");
+				    document.addEventListener("online",function(){
+				    	$scope.offline=false;
+						alert("Reconnection !");
+						document.removeEventListener("online",function(){},false);
+						location.reload();
+				}, false);
+		}, false);
 });

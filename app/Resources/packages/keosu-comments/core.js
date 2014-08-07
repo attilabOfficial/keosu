40a7283fa87787a2f421cf36b02a69bfc0d8ee5b
florent <florent.developpeur.web@gmail.com>
2014-08-07 17:22:17 +0200

    fix keosu comments

--- app/Resources/packages/keosu-comments/core.js
+++ app/Resources/packages/keosu-comments/core.js
@@ -1,32 +1,31 @@
-app.controller('comment_keosuController', function ($scope, $http, $sce, usSpinnerService) {
-
-	$scope.commentListAction = function() {
-		$http.get($scope.param.host+'service/gadget/comment/'+$scope.dataModelObjectName+'/'+$scope.objectId).success(function(data){
-			$scope.comments = data.comments;
-			$scope.connect = data.connect;
-		});
-	};
-	
-	$scope.commentAddAction = function() {
-		var data = "message="+$scope.messageComment;
-		$scope.messageComment = "";
-		$http.post($scope.param.host+'service/gadget/comment/'+$scope.dataModelObjectName+'/'+$scope.objectId,data).success(function(data){
-			$scope.comments = data.comments;
-			$scope.connect = data.connect;
-		});
-	};
-
-});
-
-app.directive('keosu-comments', function(){
+app.directive('keosuComments', function(){
 	return {
-		restrict: 'E',
-		scope: {
+		restrict : 'E',
+		scope : {
 			objectId: '=objectId',
-			dataModelObjectName: '=dataModelObjectName',
-			enableComments: '=enable',
+			patateObjectName: '=patateObjectName',
+			enableComments: '=enableComments',
 			templateName: '=templateName'
 		},
-		templateUrl: 'templates/{{ templateName }}.html'
+		templateUrl : 'plugins/keosu-comments/templates/default.html',
+		controller : function ($scope, $http, usSpinnerService) {
+
+			$scope.commentListAction = function() {
+				console.log($scope);
+				$http.get(main.host+'service/gadget/comment/'+$scope.objectName+'/'+$scope.objectId).success(function(data){
+					$scope.comments = data.comments;
+					$scope.connect = data.connect;
+				});
+			};
+
+			$scope.commentAddAction = function() {
+				var data = "message="+$scope.messageComment;
+				$scope.messageComment = "";
+				$http.post($scope.host+'service/gadget/comment/'+$scope.objectName+'/'+$scope.objectId,data).success(function(data){
+					$scope.comments = data.comments;
+					$scope.connect = data.connect;
+				});
+			};
+		}
 	};
 });

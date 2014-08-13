02578300bd35c7fb13de73891f094f44009659f4
Badboum <alexandre-marion@hotmail.fr>
2014-08-13 11:49:49 +0200

    Refactoring code for theme and template radio button.

--- web/js/master.js
+++ web/js/master.js
@@ -1,3 +1,54 @@
 $(function() {
 	$( document ).tooltip();
 });
+
+/*
+ * Create radio button with a picture for a template.
+ * (url_base / templateDir / name) is the path where the picture is.
+ */
+function templateButton(url_base, templateDir, name){
+	$( ".imgTemplate .radio" ).each(function( index ) {
+		input = $(this).find('input');
+		templateName=input.attr('value');
+		input.attr('id',templateName);
+		img = '<label for="'+templateName+'"><img src="'+url_base+'/'+templateDirdir+name+'/'+templateName+'.png" template="'+templateName+'"/></label>';
+		$(this).text("");
+		$(this).append(input);
+		$(this).append(img);
+		
+	});
+
+	$(".imgTemplate input[checked=checked]").parent(".radio").find("img").css("border","red solid 3px");
+	
+	// switch border color
+	$(".imgTemplate img").click(function(){
+		template=$(this).attr('template');
+		$("#"+template).addClass('checked','checked');
+		$(".imgTemplate img").css("border","none");
+		$(this).css("border","red solid 3px");
+	});	
+}
+
+/*
+ * Create radio button with a picture for a theme.
+ */
+function themeButton(url_base, themeDir){
+	$( ".radio" ).each(function( index ) {
+		input = $(this).find('input');
+		themeName=input.attr('value');
+		input.attr('id',themeName);
+		img='<label for="'+themeName+'"><img alt="'+themeName+'" src="'+url_base+'/'+themeDir+themeName+'/'+themeName+'.png"/></label>';
+		
+		$( this ).text("");
+		$( this ).append(input);
+		$( this ).append(img);
+	});
+
+	$(".imgtheme input[checked=checked]").parent(".radio").find("img").css("border","red solid 3px");
+	
+	// switch border color
+	$(".imgtheme img").click(function(){
+		$(".imgtheme img").css("border","none");
+		$(this).css("border","red solid 3px");
+	});
+}

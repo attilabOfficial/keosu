c553d402f570c9a54fb6115b7f369949d131b0ee
florent <florent.developpeur.web@gmail.com>
2014-08-14 15:05:10 +0200

    Add interface fonctionnal to send notification form backoffice

--- src/Keosu/Gadget/PushNotificationBundle/Controller/DefaultController.php
+++ src/Keosu/Gadget/PushNotificationBundle/Controller/DefaultController.php
@@ -2,7 +2,10 @@
 
 namespace Keosu\Gadget\PushNotificationBundle\Controller;
 
+use Keosu\Gadget\PushNotificationBundle\Entity\Devices;
+
 use RMS\PushNotificationsBundle\Message\AndroidMessage;
+use RMS\PushNotificationsBundle\Message\AppleMessage;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -10,17 +13,44 @@ class DefaultController extends Controller
 {
 	public function sendPushNotificationAction()
 	{
-		/*
-		$message = new AndroidMessage();
-		$message->setGCM(true);
-		$message->setMessage('Ça va chier !!!');
-		$message->addGCMIdentifier('APA91bHXz-kwdpa5byUQlkFaqEWjrmZWtCnLqe2Dy9N5rOz8Uak5YPtRHoiIb5s5tegFMm5N8c6LXcTyQ8lg8kRNCZbjQ5e9XYHJaOHiM4e1vsgCoHdzuaoYb-Y4sOYWL35MU7IxfqgFInVl5eiUtHTzM3LAYOV74g');
-		echo $this->container->get('keosu.plugin.push.android.push')->send($message);
-		echo '<pre>';
-		print_r($this->container->get('keosu.plugin.push.android.push')->getResponses());
-		echo '</pre>';
-		$message->addGCMIdentifier('APA91bGr6YrmBwBlD_7UHDXFymq1Kx3wKg7jHahv6bZW68sOMoA7OrdastLixqcxNryLYoyC79G4J4K0AeKhe501noZpFE0DlKG9WJYKvvzXzdZ_LNzoSUiYFXmv7ejgpGXP7v0kHcoIylFYMW30XZUnQMm59GPpBQ');
-		print_r($this->container->get('keosu.plugin.push.notification.service')->send($message));*/
-		return $this->render('KeosuGadgetPushNotificationBundle:Default:index.html.twig', array('name' => 'patate'));
+		$em = $this->get('doctrine')->getManager();
+		$request = $this->get('request');
+		$error = '';$valid = false;
+
+		$formBuilder = $this->createFormBuilder();
+		$formBuilder->add('message', 'textarea', array(
+						'attr' => array('maxlength' => 64)
+		));
+		$form = $formBuilder->getForm();
+
+		if($request->getMethod() == 'POST') {
+			$form->bind($request);
+			if($form->isValid()) {
+				try {
+					$pushService = $this->container->get('keosu.plugin.push.notification.service');
+					$devices = $em->getRepository('KeosuGadgetPushNotificationBundle:Devices');
+					$androidMessage = new AndroidMessage();
+					$androidMessage->setGCM(true);
+					$appleMessage = new AppleMessage();
+					foreach($devices as $device) {
+						if($device->getType() === Devices::TYPE_APPLE) {
+							$appleMessage->setDeviceIdentifier($device->getToken());
+							//$pushService->send($appleMessage);
+						} else if($device->getType() === Devices::TYPE_ANDROID) {
+							$androidMessage->addGCMIdentifier($device->getToken());
+						}
+					}
+					$pushService->send($androidMessage);
+					$valid = true;
+				} catch(\Exception $e) {
+					$error = $e->getMessage();
+				}
+			}
+		}
+		return $this->render('KeosuGadgetPushNotificationBundle:Default:index.html.twig', array(
+									'form' => $form->createView(),
+									'error' => $error,
+									'valid' => $valid
+							));
 	}
 }

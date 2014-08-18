28a6aac70028719f9c957b556b776aca7da01a6e
florent <florent.developpeur.web@gmail.com>
2014-08-18 10:53:41 +0200

    package manager indicate if there is a JSON error in package.json

--- src/Keosu/CoreBundle/Service/PackageManager.php
+++ src/Keosu/CoreBundle/Service/PackageManager.php
@@ -183,9 +183,6 @@ class PackageManager {
 	 */
 	public function getConfigPackage($packageNameOrLocation)
 	{
-		// TODO lock php execution
-		// TODO test json
-
 		// not a path
 		if(!strstr($packageNameOrLocation,'/')) {
 			$package = $this->findPackage($packageNameOrLocation);
@@ -203,7 +200,26 @@ class PackageManager {
 				$packageLocation = $packageNameOrLocation;
 		}
 		
-		return \json_decode(\file_get_contents($packageLocation.'/package.json'),true);
+		$ret = \json_decode(\file_get_contents($packageLocation.'/package.json'),true);
+
+		if (!function_exists('json_last_error_msg')) {
+			function json_last_error_msg() {
+				static $errors = array(
+					JSON_ERROR_NONE             => null,
+					JSON_ERROR_DEPTH            => 'Maximum stack depth exceeded',
+					JSON_ERROR_STATE_MISMATCH   => 'Underflow or the modes mismatch',
+					JSON_ERROR_CTRL_CHAR        => 'Unexpected control character found',
+					JSON_ERROR_SYNTAX           => 'Syntax error, malformed JSON',
+					JSON_ERROR_UTF8             => 'Malformed UTF-8 characters, possibly incorrectly encoded'
+				);
+				$error = json_last_error();
+				return array_key_exists($error, $errors) ? $errors[$error] : "Unknown error ({$error})";
+			}
+		}
+		if(json_last_error() !== JSON_ERROR_NONE) {
+			throw new \LogicException('Unable to decode your package.json for package : '.$packageNameOrLocation.' because : '.json_last_error_msg());
+		}
+		return $ret;
 	}
 
 	/**

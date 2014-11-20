60685ddf2edd1b391c0d1e102864b96f1f76fa73
Quénan Chever <qch@keosu.com>
2014-11-20 11:33:51 +0100

    replaced base_keosu theme's icons, now icons are stored into theme directories

--- app/Resources/packages/Gadget-keosu-menu/core.js
+++ app/Resources/packages/Gadget-keosu-menu/core.js
@@ -16,12 +16,16 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ************************************************************************/
 app.controller('keosu-menuController', function ($scope, $http,$location) {
-	$scope.init = function (params) {
+	$scope.init = function(params) {
 		$scope.pages = params.gadgetParam.pages;
 	};
 
+    $scope.getPath = function(icon, page) {
+        return $location.path() == '/Page/'+page ? 'theme/icons/active/'+icon : 'theme/icons/'+icon
+    }
+
 	// @see https://stackoverflow.com/questions/12592472/how-to-highlight-a-current-menu-item-in-angularjs
 	$scope.getClass = function(page) {
-		return $location.path() == "/Page/"+page ? "active" : ""
+		return $location.path() == '/Page/'+page ? 'active' : ''
 	}
 });

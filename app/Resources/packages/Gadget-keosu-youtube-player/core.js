0f1aae57beaa983a24f599418124d2099036eb56
Jeremy Maignan <jeremy.maignan@epitech.eu>
2014-12-18 17:23:05 +0100

    add plugin and gadget youtube

---
+++ app/Resources/packages/Gadget-keosu-youtube-player/core.js
@@ -0,0 +1,6 @@
+app.controller('keosu-youtube-playerController', function ($scope) {
+
+	$scope.init = function (params){
+		$scope.id = params.gadgetParam.url; 
+	};
+});
---
+++ app/Resources/packages/Plugin-keosu-youtube/core.js
@@ -0,0 +1,29 @@
+app.directive('pluginKeosuYoutube', function($window) {
+  return {
+    restrict: "E",
+    scope: {
+      height:   "@",
+      width:    "@",
+      id:  "@"  
+    },
+
+    template: '<div></div>',
+
+    link: function(scope, element, attrs) {
+      var tag = document.createElement('script');
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      var player;
+
+      $window.onYouTubeIframeAPIReady = function() {
+        player = new YT.Player(element.children()[0], {
+          height: scope.height,
+          width: scope.width,
+          videoId: scope.id
+        });
+      };
+    },  
+  }
+});

7669a2f7f14b04de56efea766715f16acea8a9a2
Jeremy Maignan <jeremy.maignan@epitech.eu>
2015-02-05 17:01:22 +0100

    add mailto: for the mail in the contact-us gadget

--- app/Resources/packages/Gadget-keosu-contact-us/core.js
+++ app/Resources/packages/Gadget-keosu-contact-us/core.js
@@ -7,7 +7,6 @@ app.controller('keosu-contact-usController', function ($scope, cacheManagerServi
         $scope.address      = $scope.param.gadgetParam.address;
         $scope.phone        = $scope.param.gadgetParam.phone;
         $scope.email        = $scope.param.gadgetParam.email;
-
 		cacheManagerService.get($scope.param.host+'service/gadget/picture/'+$scope.param.gadgetId).success( function (data) {
 			$scope.image = data.path;
 		});

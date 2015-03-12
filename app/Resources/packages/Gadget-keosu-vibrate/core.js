c25bd9d6fe4d8a2596cfa1f3345ce13a1f96a07f
Vincent Le Borgne <vleborgne@gmail.com>
2015-03-12 11:07:05 +0100

    Remove gadgets

--- app/Resources/packages/Gadget-keosu-vibrate/core.js
+++
@@ -1,9 +0,0 @@
-app.controller('keosu-vibrateController', function ($scope) {
-
-	$scope.init = function (){
-	};
-
-	$scope.vibrate = function (){
-		navigator.vibrate(1000);//vibration duration in ms
-	};
-});

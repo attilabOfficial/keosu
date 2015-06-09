42dfab61cc2a6b0d4a14e5252d52c7c2561eaeea
root <root@ks211967.kimsufi.com>
2015-06-09 14:48:30 +0200

    push update

--- src/Keosu/Gadget/PushNotificationBundle/Service/AndroidGCMNotification.php
+++ src/Keosu/Gadget/PushNotificationBundle/Service/AndroidGCMNotification.php
@@ -7,6 +7,7 @@ use Keosu\Gadget\PushNotificationBundle\KeosuGadgetPushNotificationBundle;
 class AndroidGCMNotification extends \RMS\PushNotificationsBundle\Service\OS\AndroidGCMNotification {
 
 	public function __construct($container) {
+		$logger = $container->get('logger');
 		$em = $container->get('doctrine')->getManager();
 		$appId = $container->get('keosu_core.curapp')->getCurApp();
 		$app = $em->getRepository('KeosuCoreBundle:App')->find($appId);
@@ -14,7 +15,7 @@ class AndroidGCMNotification extends \RMS\PushNotificationsBundle\Service\OS\And
 		$googleUseMultiCurl = $app->getConfigPackages()[KeosuGadgetPushNotificationBundle::PACKAGE_NAME]['googleUseMultiCurl'];
 		if($googleGcmApiKey === null)
 			throw new \LogicException('Can\'t send message because google gcm api key in not defined');
-		parent::__construct($googleGcmApiKey,$googleUseMultiCurl);
+		parent::__construct($googleGcmApiKey,$googleUseMultiCurl,60,$logger);
 	}
 
 }

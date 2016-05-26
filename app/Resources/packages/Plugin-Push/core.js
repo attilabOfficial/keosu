4c329c1c479d5d899708229138edc074237a1cf2
PHALIPPOU Zoe <phalip_z@epitech.eu>
2016-05-26 14:21:04 +0200

    Upgrade : Plugin Push.

--- app/Resources/packages/Plugin-Push/core.js
+++ app/Resources/packages/Plugin-Push/core.js
@@ -1,112 +1,79 @@
+/************************************************************************
+ Keosu is an open source CMS for mobile app
+ Copyright (C) 2013  Vincent Le Borgne
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as
+ published by the Free Software Foundation, either version 3 of the
+ License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ************************************************************************/
 // Don't forget to take a look at exportListener.php before edit this file
-app.directive('keosuPush', function(){
-	return {
-		restrict : 'E',
-		templateUrl : 'plugins/keosu-push/templates/default.html',
-		controller : ['$scope','$http', function ($scope, $http) {
 
-			$http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+app.directive('keosuPush', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'plugins/keosu-push/templates/default.html',
+        controller: ['$scope', '$http', function ($scope, $http) {
 
-			$scope.init = function() {
-				console.log('function init');
-				document.addEventListener('deviceready', function() {
-					console.log('i\'m ready');
-					// if push notification is enabled
-					pushNotification = window.plugins.pushNotification;
-					if(typeof(device) != 'undefined' && typeof(pushNotificationData) != 'undefined') {
-						$http.get('data/globalParam.json').success(function(data) {
-							$scope.host = data.host;
-							if(device.platform == 'android' || device.platform == 'Android' || device.platform == "amazon-fireos") {
-								$scope.androidPush();
-							} else {
-								$scope.applePush();
-							}
-						});
-					}
-				},false);
-			};
+            $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
 
-			////////////////////////
-			// Android part
-			////////////////////////
-			$scope.androidPush = function() {
-				appAndroid = {
-					successHandler: function(result) {
-						console.log("Push success : "+result);
-					},
-					errorHandler: function(error) {
-						console.log("Push error : "+error);
-					},
-					onNotification: function(e) {
-						switch( e.event ) {
-							case 'registered':
-							if ( e.regid.length > 0 ) {
-								$http.post($scope.host+'service/push/plugin/addDevice/2','token='+e.regid);
-							}
-							break;
+            console.log('function init');
+            $scope.init = function () {
+                document.addEventListener('deviceready', function () {
+                    console.log('i\'m ready');
 
-							case 'message':
-							// this is the actual push notification. its format depends on the data model from the push server
-							console.log('message = '+e.message+' msgcnt = '+e.msgcnt);
-							break;
+                    $http.get('data/globalParam.json').success(function (data) {
+                        console.log(data);
+                    });
 
-							case 'error':
-							console.log('GCM error = '+e.msg);
-							break;
+                    if (typeof(device) != 'undefined' && typeof(PushNotification) != 'undefined') {
+                        $http.get('data/globalParam.json').success(function (data) {
+                            $scope.host = data.host;
+                            var push = PushNotification.init({
+                                android: {
+                                    senderID: pushNotificationData.senderID + "", 'ecb': "appAndroid.onNotification"
+                                },
+                                ios: {
+                                    alert: "true",
+                                    badge: "true",
+                                    sound: "true"
+                                },
+                                windows: {}
+                            });
 
-							default:
-							console.log('An unknown GCM event has occurred');
-							break;
-						}
-					}
-				};
+                            push.on('registration', function (data) {
+                                var deviceId = (device.platform == 'android' || device.platform == 'Android' || device.platform == "amazon-fireos") ? 2 : 1;
+                                $http.post($scope.host + 'service/push/plugin/addDevice/' + deviceId, 'token=' + data.registrationId);
+                            });
 
-				pushNotification.register(
-					appAndroid.successHandler,
-					appAndroid.errorHandler,
-					{'senderID':pushNotificationData.senderID+"",'ecb': "appAndroid.onNotification" }
-				);
-			};
-		
-			/////////////////////
-			// Apple part
-			/////////////////////
-			$scope.applePush = function() {
-				console.log('I\'m an Iphone...');
-				appPushNotification = {
-					tokenHandler: function(result) {
-						console.log('iphone id');
-						console.log(result);
-						if( result.length > 0 ) {
-							$http.post($scope.host+'service/push/plugin/addDevice/1','token='+result);
-						}
-					},
-					errorHandler: function(error){console.log('the eoor is '+error)},
-					onNotificationAPN: function(event) {
-						if( event.alert )
-							navigator.notification.alert(event.alert);
-						if( event.sound ) {
-							var snd = new Media(event.sound);
-							snd.play();
-						}
-						if( event.badge )
-							pushNotification.setApplicationIconBadgeNumber(successHandler, errorHandler, event.badge);
-					}
-				};
+                            push.on('notification', function (data) {
+                                function onConfirm(buttonIndex) {
+                                }
 
-				pushNotification.register(
-					appPushNotification.tokenHandler,
-					appPushNotification.errorHandler,
-					{
-						'badge' : 'true',
-						'sound' : 'true',
-						'alert' : 'true',
-						'ecb'   : 'appPushNotification.onNotificationAPN'
-					}
-				);
-			};
+                                navigator.notification.confirm(
+                                    data.message,   // message
+                                    onConfirm,      // callback to invoke with index of button pressed
+                                    'Notification', // title
+                                    ['OK']          // buttonLabels
+                                );
+                            });
 
-		}]
-	};
+                            push.on('error', function (e) {
+                            });
+                        });
+                    }
+
+                });
+            };
+        }]
+    };
 });
 
--- src/Keosu/Gadget/PushNotificationBundle/Controller/DefaultController.php
+++ src/Keosu/Gadget/PushNotificationBundle/Controller/DefaultController.php
@@ -9,55 +9,59 @@ use RMS\PushNotificationsBundle\Message\AppleMessage;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
 
 class DefaultController extends Controller
 {
 	public function sendPushNotificationAction(Request $request)
 	{
 		$em = $this->get('doctrine')->getManager();
-		$error = '';$valid = false;
+		$error = '';
+		$valid = false;
 
 		$formBuilder = $this->createFormBuilder();
-		$formBuilder->add('message', 'textarea', array(
-						'attr' => array('maxlength' => 64)
+		$formBuilder->add('message', TextareaType::class, array(
+			'attr' => array('maxlength' => 64)
 		));
 		$form = $formBuilder->getForm();
 
-		if($request->getMethod() == 'POST') {
-			$form->bind($request);
-			if($form->isValid()) {
-				try {
-					$pushService = $this->container->get('keosu.plugin.push.notification.service');
-					$devices = $em->getRepository('KeosuGadgetPushNotificationBundle:Devices')->findAll();
-					
-					$androidMessage = new AndroidMessage();
-					$androidMessage->setGCM(true);
-					$androidMessage->setMessage($form['message']->getData());
-					
-					$appleMessage = new AppleMessage();
-					$appleMessage->setMessage($form['message']->getData());
-					$appleMessage->setAPSBadge(1);
-					$appleMessage->setAPSSound('default');
-					
-					foreach($devices as $device) {
-						if($device->getType() === Devices::TYPE_APPLE) {
-							$appleMessage->setDeviceIdentifier($device->getToken());
-							$pushService->send($appleMessage);
-						} else if($device->getType() === Devices::TYPE_ANDROID) {
-							$androidMessage->addGCMIdentifier($device->getToken());
-						}
+		$form->handleRequest($request);
+		if ($form->isValid()) {
+			try {
+				$pushService = $this->container->get('keosu.plugin.push.notification.service');
+				$devices = $em->getRepository('KeosuGadgetPushNotificationBundle:Devices')->findAll();
+
+				$androidMessage = new AndroidMessage();
+				$androidMessage->setGCM(true);
+				$androidMessage->setMessage($form['message']->getData());
+
+				$appleMessage = new AppleMessage();
+				$appleMessage->setMessage($form['message']->getData());
+				$appleMessage->setAPSBadge(1);
+				$appleMessage->setAPSSound('default');
+
+
+
+				foreach ($devices as $device) {
+					if ($device->getType() === Devices::TYPE_APPLE) {
+						$appleMessage->setDeviceIdentifier($device->getToken());
+						$pushService->send($appleMessage);
+					} else if ($device->getType() === Devices::TYPE_ANDROID) {
+						$androidMessage->addGCMIdentifier($device->getToken());
 					}
-					$pushService->send($androidMessage);
-					$valid = true;
-				} catch(\Exception $e) {
-					$error = $e->getMessage();
 				}
+				$pushService->send($androidMessage);
+				$valid = true;
+			} catch (\Exception $e) {
+				$error = $e->getMessage();
 			}
+
 		}
 		return $this->render('KeosuGadgetPushNotificationBundle:Default:index.html.twig', array(
-									'form'  => $form->createView(),
-									'error' => $error,
-									'valid' => $valid
-							));
+			'form' => $form->createView(),
+			'error' => $error,
+			'valid' => $valid
+		));
 	}
 }
